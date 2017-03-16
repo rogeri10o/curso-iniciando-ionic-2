02bd6febd0e3e4b02afbfa4b0595651e141dd9b3
rogeri10o <rogeri10o@yahoo.com.br>
2017-03-16 01:02:45 -0300

    capitulo 3 aula 4 [v2]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,25 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
+  pages: Array<{component: any, title: string, icon: string}>;
+
   rootPage = HomePage;
 
   constructor(platform: Platform) {
+
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'},
+      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +29,9 @@ export class MyApp {
       Splashscreen.hide();
     });
   }
+
+  openPage(page: any): void {
+    this.rootPage = page.component;
+  }
+
 }
